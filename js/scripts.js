0e684c0963dc38d66b32921eebf8fa5b4d435a20
friendo9876 <friendo9876>
2017-02-15 14:48:31 -0500

    fixed js not using jquery anymore

--- js/scripts.js
+++ js/scripts.js
@@ -1,62 +1,56 @@
-$( document ).ready(function()
-{
-  var snareSound = document.createElement('audio');
-  snareSound.setAttribute('src', 'audio/snare.wav');
+var snareSound = document.createElement('audio');
+snareSound.setAttribute('src', 'audio/snare.wav');
 
-  var openhatSound = document.createElement('audio');
-  openhatSound.setAttribute('src', 'audio/openhat.wav');
+var openhatSound = document.createElement('audio');
+openhatSound.setAttribute('src', 'audio/openhat.wav');
 
-  var bassdrumSound = document.createElement('audio');
-  bassdrumSound.setAttribute('src', 'audio/kick.wav');
+var bassdrumSound = document.createElement('audio');
+bassdrumSound.setAttribute('src', 'audio/kick.wav');
 
-  var hihatSound = document.createElement('audio');
-  hihatSound.setAttribute('src', 'audio/hihat.wav');
+var hihatSound = document.createElement('audio');
+hihatSound.setAttribute('src', 'audio/hihat.wav');
 
-  $('#snare').click(function()
-  {
-    snareSound.pause();
-    snareSound.currentTime = 0;
-    snareSound.play();
-  });
+document.getElementById('snare').addEventListener('click', snarefunction1);
+document.getElementById('snare').addEventListener('mouseover', snarefunction2);
 
-  $('#snare').mouseover(function()
-  {
-    snareSound.play();
-  });
+document.getElementById('openhat').addEventListener('click', openhatfunction1);
+document.getElementById('openhat').addEventListener('mouseover', openhatfunction2);
 
-  $('#openhat').click(function()
-  {
-    openhatSound.pause();
-    openhatSound.currentTime =0;
-    openhatSound.play();
-  });
+document.getElementById('hihat').addEventListener('click', hihatfunction1);
+document.getElementById('hihat').addEventListener('mouseover', hihatfunction2);
 
-  $('#openhat').mouseover(function()
-  {
-    openhatSound.play();
-  });
+document.getElementById('bass').addEventListener('click', bassfunction1);
+document.getElementById('bass').addEventListener('mouseover', bassfunction2);
 
-  $('#hihat').click(function()
-  {
-    hihatSound.pause();
-    hihatSound.currentTime = 0;
-    hihatSound.play();
-  });
-
-  $('#hihat').mouseover(function()
-  {
-    hihatSound.play();
-  });
-
-  $('#bass').click(function()
-  {
-    bassdrumSound.pause();
-    bassdrumSound.currentTime =0;
-    bassdrumSound.play();
-  });
-
-  $('#bass').mouseover(function()
-  {
-    bassdrumSound.play();
-  });
-});
+function snarefunction1 () {
+  snareSound.pause();
+  snareSound.currentTime = 0;
+  snareSound.play();
+}
+function snarefunction2 () {
+  snareSound.play();
+}
+function openhatfunction1 () {
+  openhatSound.pause();
+  openhatSound.currentTime = 0;
+  openhatSound.play();
+}
+function openhatfunction2 () {
+  openhatSound.play();
+}
+function hihatfunction1 () {
+  hihatSound.pause();
+  hihatSound.currentTime = 0;
+  hihatSound.play();
+}
+function hihatfunction2 () {
+  hihatSound.play();
+}
+function bassfunction1 () {
+  bassdrumSound.pause();
+  bassdrumSound.currentTime = 0;
+  bassdrumSound.play();
+}
+function bassfunction2 () {
+  bassdrumSound.play();
+}
