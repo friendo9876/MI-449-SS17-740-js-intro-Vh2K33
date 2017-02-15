eac291f4006ea3f5e2d28fe823a4c43c4c820d6b
friendo9876 <friendo9876>
2017-02-14 21:58:37 -0500

    fixed it so when you click it immediately restarted the sound

--- js/scripts.js
+++ js/scripts.js
@@ -1,53 +1,62 @@
-$( document ).ready(function() {
-    var snareSound = document.createElement('audio');
-    snareSound.setAttribute('src', 'audio/snare.wav');
+$( document ).ready(function()
+{
+  var snareSound = document.createElement('audio');
+  snareSound.setAttribute('src', 'audio/snare.wav');
 
-    var openhatSound = document.createElement('audio');
-    openhatSound.setAttribute('src', 'audio/openhat.wav');
+  var openhatSound = document.createElement('audio');
+  openhatSound.setAttribute('src', 'audio/openhat.wav');
 
-    var bassdrumSound = document.createElement('audio');
-    bassdrumSound.setAttribute('src', 'audio/kick.wav');
+  var bassdrumSound = document.createElement('audio');
+  bassdrumSound.setAttribute('src', 'audio/kick.wav');
 
-    var hihatSound = document.createElement('audio');
-    hihatSound.setAttribute('src', 'audio/hihat.wav');
+  var hihatSound = document.createElement('audio');
+  hihatSound.setAttribute('src', 'audio/hihat.wav');
 
-    $('#snare').click(function()
-    {
-       snareSound.play();
-    });
+  $('#snare').click(function()
+  {
+    snareSound.pause();
+    snareSound.currentTime = 0;
+    snareSound.play();
+  });
 
-    $('#snare').mouseover(function()
-    {
-       snareSound.play();
-    });
+  $('#snare').mouseover(function()
+  {
+    snareSound.play();
+  });
 
-    $('#openhat').click(function()
-    {
-       openhatSound.play();
-    });
+  $('#openhat').click(function()
+  {
+    openhatSound.pause();
+    openhatSound.currentTime =0;
+    openhatSound.play();
+  });
 
-    $('#openhat').mouseover(function()
-    {
-       openhatSound.play();
-    });
+  $('#openhat').mouseover(function()
+  {
+    openhatSound.play();
+  });
 
-    $('#hihat').click(function()
-    {
-       hihatSound.play();
-    });
+  $('#hihat').click(function()
+  {
+    hihatSound.pause();
+    hihatSound.currentTime = 0;
+    hihatSound.play();
+  });
 
-    $('#hihat').mouseover(function()
-    {
-       hihatSound.play();
-    });
+  $('#hihat').mouseover(function()
+  {
+    hihatSound.play();
+  });
 
-    $('#bass').click(function()
-    {
-       bassdrumSound.play();
-    });
+  $('#bass').click(function()
+  {
+    bassdrumSound.pause();
+    bassdrumSound.currentTime =0;
+    bassdrumSound.play();
+  });
 
-    $('#bass').mouseover(function()
-    {
-       bassdrumSound.play();
-    });
+  $('#bass').mouseover(function()
+  {
+    bassdrumSound.play();
+  });
 });
