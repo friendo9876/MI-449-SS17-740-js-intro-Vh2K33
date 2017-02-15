a8fcf2dd3c7b982f48856879ff92676e1674a365
friendo9876 <friendo9876>
2017-02-15 16:41:47 -0500

    fixed js

--- js/scripts.js
+++ js/scripts.js
@@ -1,56 +1,56 @@
-var snareSound = document.createElement('audio');
-snareSound.setAttribute('src', 'audio/snare.wav');
+var snareSound = document.createElement('audio')
+snareSound.setAttribute('src', 'audio/snare.wav')
 
-var openhatSound = document.createElement('audio');
-openhatSound.setAttribute('src', 'audio/openhat.wav');
+var openhatSound = document.createElement('audio')
+openhatSound.setAttribute('src', 'audio/openhat.wav')
 
-var bassdrumSound = document.createElement('audio');
-bassdrumSound.setAttribute('src', 'audio/kick.wav');
+var bassdrumSound = document.createElement('audio')
+bassdrumSound.setAttribute('src', 'audio/kick.wav')
 
-var hihatSound = document.createElement('audio');
-hihatSound.setAttribute('src', 'audio/hihat.wav');
+var hihatSound = document.createElement('audio')
+hihatSound.setAttribute('src', 'audio/hihat.wav')
 
-document.getElementById('snare').addEventListener('click', snarefunction1);
-document.getElementById('snare').addEventListener('mouseover', snarefunction2);
+document.getElementById('snare').addEventListener('click', snarefunction1)
+document.getElementById('snare').addEventListener('mouseover', snarefunction2)
 
-document.getElementById('openhat').addEventListener('click', openhatfunction1);
-document.getElementById('openhat').addEventListener('mouseover', openhatfunction2);
+document.getElementById('openhat').addEventListener('click', openhatfunction1)
+document.getElementById('openhat').addEventListener('mouseover', openhatfunction2)
 
-document.getElementById('hihat').addEventListener('click', hihatfunction1);
-document.getElementById('hihat').addEventListener('mouseover', hihatfunction2);
+document.getElementById('hihat').addEventListener('click', hihatfunction1)
+document.getElementById('hihat').addEventListener('mouseover', hihatfunction2)
 
-document.getElementById('bass').addEventListener('click', bassfunction1);
-document.getElementById('bass').addEventListener('mouseover', bassfunction2);
+document.getElementById('bass').addEventListener('click', bassfunction1)
+document.getElementById('bass').addEventListener('mouseover', bassfunction2)
 
 function snarefunction1 () {
-  snareSound.pause();
-  snareSound.currentTime = 0;
-  snareSound.play();
+  snareSound.pause()
+  snareSound.currentTime = 0
+  snareSound.play()
 }
 function snarefunction2 () {
-  snareSound.play();
+  snareSound.play()
 }
 function openhatfunction1 () {
-  openhatSound.pause();
-  openhatSound.currentTime = 0;
-  openhatSound.play();
+  openhatSound.pause()
+  openhatSound.currentTime = 0
+  openhatSound.play()
 }
 function openhatfunction2 () {
-  openhatSound.play();
+  openhatSound.play()
 }
 function hihatfunction1 () {
-  hihatSound.pause();
-  hihatSound.currentTime = 0;
-  hihatSound.play();
+  hihatSound.pause()
+  hihatSound.currentTime = 0
+  hihatSound.play()
 }
 function hihatfunction2 () {
-  hihatSound.play();
+  hihatSound.play()
 }
 function bassfunction1 () {
-  bassdrumSound.pause();
-  bassdrumSound.currentTime = 0;
-  bassdrumSound.play();
+  bassdrumSound.pause()
+  bassdrumSound.currentTime = 0
+  bassdrumSound.play()
 }
 function bassfunction2 () {
-  bassdrumSound.play();
+  bassdrumSound.play()
 }
